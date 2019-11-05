5334be1b1c49ab7e0fb00def53806559104f66e6
zk <271226192@qq.com>
2019-11-05 09:56:23 +0800

    提交: 完成第四章服务发现例子

---
+++ microservice-discovery-eureka/src/main/java/top/jasonkayzk/microservicediscoveryeureka/MicroserviceDiscoveryEurekaApplication.java
@@ -0,0 +1,18 @@
+package top.jasonkayzk.microservicediscoveryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka微服务发现
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroserviceDiscoveryEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDiscoveryEurekaApplication.class, args);
+    }
+
+}
---
+++ microservice-provider-user/src/main/java/top/jasonkayzk/microservicesimpleprovideruser/MicroserviceProviderUserApplication.java
@@ -0,0 +1,15 @@
+package top.jasonkayzk.microservicesimpleprovideruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient(autoRegister = true)
+@SpringBootApplication
+public class MicroserviceProviderUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceProviderUserApplication.class, args);
+    }
+
+}
